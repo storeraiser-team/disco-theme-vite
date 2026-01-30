7f752df1f66880ee6c5df640f44309ab71c2fa98
Thalison Botelho <thalison@disco-tec.com>
2026-01-30 11:55:44 -0300

    feat: Implement shipping rate simulation and migrate slider library from Splide to Swiper.

--- frontend/entrypoints/store.js
+++ frontend/entrypoints/store.js
@@ -1,8 +1,7 @@
 import discoAlpine from "alpinejs";
 import discoMask from '@alpinejs/mask'
-import discoSplide from "@splidejs/splide";
-
-import "@splidejs/splide/css/core";
+import discoSwiper from "swiper/bundle";
+import 'swiper/css/bundle';
 
 discoAlpine.plugin(discoMask);
 
@@ -216,18 +215,146 @@ discoAlpine.store("product", {
   }
 });
 
-discoAlpine.store("slider", {
+discoAlpine.data('shipping', () => ({
+  loading: false,
+  message: '',
+  zipcode: '',
+  zipcodeMask: /^\d{5}-\d{3}$/,
+  rates: [],
+  shippingAddress: {
+    zip: '',
+    country: Shopify.country,
+    province: '',
+    city: '',
+    address1: '',
+  },
+  showShippingInput: false,
+  async simulateRates(productId) {
+    this.rates = [];
+    this.message = '';
+
+    if (!this.zipcode || !this.zipcodeMask.test(this.zipcode)) {
+      this.message = 'CEP inválido';
+      return;
+    }
+
+    this.loading = true;
+
+    const zip = this.zipcode;
+    const country = Shopify.country;
+    let quantity = 1;
+
+    if (productId) {
+      const addItem = await this.$store.cart.addItems([{ id: productId, quantity }], { showCart: false });
+      quantity = ((addItem.items || []).find((item) => item.id === productId)?.quantity || 1) - 1;
+    }
+
+    if (country === 'BR') {
+      try {
+        const cachedAddressKey = `address_${this.zipcode}`;
+        const cachedAddress = localStorage.getItem(cachedAddressKey);
+        let address;
+
+        if (cachedAddress) {
+          address = JSON.parse(cachedAddress);
+        } else {
+          const addressResponse = await fetch(`https://viacep.com.br/ws/${this.zipcode}/json/`);
+          address = await addressResponse.json();
+
+          if (address.erro) {
+            this.message = 'CEP não encontrado';
+            this.loading = false;
+            return;
+          }
+
+          localStorage.setItem(cachedAddressKey, JSON.stringify(address));
+        }
+
+        this.shippingAddress = {
+          zip,
+          country,
+          province: address.estado,
+          city: address.localidade,
+          address1: address.logradouro,
+        };
+      } catch (err) {}
+    }
+
+    const fetchOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ shipping_address: this.shippingAddress }),
+    };
+
+    await fetch(`/cart/prepare_shipping_rates.json`, fetchOptions);
+
+    const shippingRatesResponse = await fetch(`/cart/shipping_rates.json`, fetchOptions);
+    const shippingRates = await shippingRatesResponse.json();
+
+    this.rates = shippingRates.shipping_rates.map((shippingRate) => {
+      const price = Math.round(parseFloat(shippingRate.price) * 100);
+      const comparePrice = Math.round(parseFloat(shippingRate.compare_price || shippingRate.price) * 100);
+
+      const today = new Date();
+      const tomorrow = new Date(today);
+      tomorrow.setDate(tomorrow.getDate() + 1);
+
+      const [startDate, endDate] = (shippingRate.delivery_range || [today, today]).map((date) => new Date(date));
+      const formatDate = (date) => date.toLocaleDateString(Shopify.locale, { day: '2-digit', month: '2-digit' });
+      const deliveryDate = formatDate(startDate);
+
+      let description = `Previsão de entrega entre ${deliveryDate} e ${formatDate(endDate)}`;
+
+      if (startDate.toDateString() === today.toDateString()) {
+        description = `${shippingRate.delivery_range ? 'Previsão de entrega' : 'Retirada'} hoje (${deliveryDate})`;
+      } else if (startDate.toDateString() === tomorrow.toDateString()) {
+        description = `Previsão de entrega amanhã (${deliveryDate})`;
+      } else if (startDate.getTime() === endDate.getTime()) {
+        description = `Previsão de entrega dia ${deliveryDate}`;
+      }
+
+      return {
+        name: shippingRate.presentment_name,
+        description,
+        deliveryDate,
+        comparePrice,
+        price,
+      };
+    });
+
+    if (productId) {
+      const updates = { [productId]: quantity };
+      await this.$store.cart.updateItems(updates);
+    }
+
+    this.loading = false;
+  },
+  toggleShippingInput() {
+    if (this.rates.length > 0) {
+      this.resetShipping();
+    }
+    this.showShippingInput = !this.showShippingInput;
+  },
+  resetShipping() {
+    this.rates = [];
+    this.shippingAddress = { zip: '', country: 'BR', province: '', city: '', address1: '' };
+    this.zipcode = '';
+  }
+}));
+
+
+discoAlpine.store("swiper", {
   mount(el) {
     const queryClasses = el?.id
-      ? `#${el.id} .splide`
-      : ".shopify-section .splide";
+      ? `#${el.id} .swiper`
+      : ".shopify-section .swiper";
     const spls = document.querySelectorAll(queryClasses);
 
     // biome-ignore lint/complexity/noForEach: <explanation>
     spls.forEach((spl) => {
-      new discoSplide(spl, {
+      new discoSwiper(spl, {
         mediaQuery: "min",
-      }).mount();
+      });
     });
   },
 });
@@ -263,19 +390,20 @@ discoAlpine.directive(
 document.addEventListener("alpine:initialized", () => {
   if (Shopify?.designMode) {
     document.addEventListener("shopify:section:load", (section) => {
-      discoAlpine.store("slider").mount();
+      discoAlpine.store("swiper").mount();
     });
     document.addEventListener("shopify:section:reorder", () => {
-      discoAlpine.store("slider").mount();
+      discoAlpine.store("swiper").mount();
     });
   }
 });
 
 discoAlpine.prefix("disco-");
 window.discoAlpine = discoAlpine;
+window.discoSwiper = discoSwiper;
 
 document.addEventListener("DOMContentLoaded", () => {
-  discoAlpine.store("slider").mount();
+  discoAlpine.store("swiper").mount();
 });
 
 discoAlpine.start();
